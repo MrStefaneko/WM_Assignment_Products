c65da3ed8c313a1075736c1b64b9d195745fe9fc
STEFAN\Stefan <stefaneka@yahoo.com>
2018-09-22 18:36:34 +0200

    Add asp.NET MVC project

---
+++ WM_Assignment_Products/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WM_Assignment_Products.MvcApplication" Language="C#" %>
---
+++ WM_Assignment_Products/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WM_Assignment_Products.Startup))]
+namespace WM_Assignment_Products
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+           
+        }
+    }
+}
