493c669a5851ac417618a2380270404d4fddb3e5
STEFAN\Stefan <stefaneka@yahoo.com>
2018-09-23 10:57:31 +0200

    Add model, controlers and views for printing data from DB and JSON file to HTML table

---
+++ WM_Assignment_Products/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WM_Assignment_Products.Models;
+
+namespace WM_Assignment_Products.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ActionResult PrintProductsDB()
+        {
+            ProductContext productContext = new ProductContext();
+            //Product products = productContext.Products.SingleOrDefault(pr => pr.ID == 15);
+            List<Product> products = new List<Product>();
+            products = productContext.Products.ToList();
+            ViewBag.Products = products;
+            return View();
+        }
+
+        public ActionResult PrintProductsJSON()
+        {
+            using (StreamReader sw = new StreamReader(@"D:\Users\Stefan\documents\visual studio 2015\Projects\WM_Assignment_Products/Product.json"))
+            {
+                string json = sw.ReadToEnd();
+                ProductCollection productCollection = JsonConvert.DeserializeObject<ProductCollection>(json);
+                ViewBag.Products = productCollection.products;
+            }
+            return View();
+        }
+    }
+}
---
+++ WM_Assignment_Products/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WM_Assignment_Products.Models
+{
+    [Table("dbo.Product")]
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string Manufacturer { get; set; }
+        public string Supplier { get; set; }
+        public decimal Price { get; set; }
+    }
+}
